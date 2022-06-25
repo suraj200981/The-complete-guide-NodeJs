445f4fd7ca91b5e62f146f9c6c7b274aace3321a
suraj200981 <suraj200981@yahoo.co.uk>
2022-06-25 14:55:22 +0200

    alignment

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const server = http.createServer(function (req, res) {
     console.log(req.url, req.method, req.headers);//listen will log the request when you go to the url
    //process.exit();// hard exits after one request
 
-   res.setHeader('Content-Type', 'text/html');//setting the header
+    res.setHeader('Content-Type', 'text/html');//setting the header
    //the response is the html code when you go to the url
-   res.write('<html>');
+    res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
