a70f6f10332f530368543012f0cfca0d2ceb1cc7
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-07 17:42:32 +0200

    Parsing incomming requests with bodyParser

--- ExpressProject 1/app.js
+++ ExpressProject 1/app.js
@@ -1,23 +1,48 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express(); //creating an express app
 
-/*route for all requests*/
-app.use("/", (req, res, next) => {
-  console.log("Always runs...");
-  next();
-});
+app.use(bodyParser.urlencoded()); //using body-parser to parse the body of the request
 
 /*route for specific requests*/
 
 app.use("/add-product", (req, res, next) => {
   console.log("Products page");
-  res.send("<h1>Add new product</h1>");
-}); //next middleware
+  res.send(`
+  <html>
+  <head>
+  <title>Add Product</title>
+  </head>
+  <body>
+  <h1>Add a new product</h1>
+  <form action="/product" method="POST">
+  <input type="text" name="product">
+  <button type="submit">Add Product</button>
+  </input>
+  </form>
+  </body>
+  </html>`);
+}); //add product middleware
 
+app.use("/product", (req, res, next) => {
+  console.log(req.body.product.toString());
+  res.redirect("/"); //redirect to home page
+}); //product middleware
+
+//express will always end up at the "/" route
 app.use("/", (req, res, next) => {
-  console.log("Homepage");
-  res.send("<h1>Hello World</h1>");
-}); //next middleware
+  res.send(`
+  <html>
+  <head>
+  <title>Add Product</title>
+  </head>
+  <body>
+  <h1>Home</h1>
+  <a href='/add-product'>Add Product</a>
+  </body>
+  </html>`);
+}); //home middleware
 
 app.listen(3000);
