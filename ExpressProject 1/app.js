4d08438dec9750f40d4870f62ec52840b4fa9e12
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-07 18:42:30 +0200

    404 html page returning

--- ExpressProject 1/app.js
+++ ExpressProject 1/app.js
@@ -1,4 +1,5 @@
 /**************Imports*************/
+const path = require("path");
 
 const express = require("express");
 
@@ -18,7 +19,7 @@ app.use(shopRoutes); //using the shop routes
 
 //404 middleware
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
