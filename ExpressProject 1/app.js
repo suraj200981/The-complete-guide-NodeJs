e748dd644155a7241c1fa348ba530ee6a088672e
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-07 17:00:15 +0200

    introduction to Express request funneling

--- ExpressProject 1/app.js
+++ ExpressProject 1/app.js
@@ -1,18 +1,21 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express(); //creating an express app
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // this allows the request to continue to the next middleware
-}); //middleware
+//route for all requests
+app.use("/", (req, res, next) => {
+  console.log("Always runs...");
+  next();
+});
 
-app.use((req, res, next) => {
-  console.log("In another the middleware");
+app.use("/add-product", (req, res, next) => {
+  console.log("Products page");
+  res.send("<h1>Add new product</h1>");
+}); //next middleware
+
+app.use("/", (req, res, next) => {
+  console.log("Homepage");
   res.send("<h1>Hello World</h1>");
 }); //next middleware
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
