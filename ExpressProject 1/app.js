0c6b26e82f2bcd8b38a00a059f2dcfdd8170943e
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-07 15:17:40 +0200

    introduction to middlewares

--- ExpressProject 1/app.js
+++ ExpressProject 1/app.js
@@ -1,4 +1,18 @@
 const http = require("http");
-console.log(routes.someText);
-const server = http.createServer();
+
+const express = require("express");
+
+const app = express(); //creating an express app
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // this allows the request to continue to the next middleware
+}); //middleware
+
+app.use((req, res, next) => {
+  console.log("In another the middleware");
+  res.send("<h1>Hello World</h1>");
+}); //next middleware
+const server = http.createServer(app);
+
 server.listen(3000);
