96569b8b2da26d9a664308a5dfd88a93b58df7d2
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-07 14:22:57 +0200

    preparing directory for ExpressJS

---
+++ UsingHTTPModuleCoreNodeJS/routingAndLogging.js
@@ -0,0 +1,14 @@
+//import requesthandler
+const requestHandler = require("./UsingHTTPModuleCoreNodeJS/routes.js");
+
+const http = require("http"); //importing http module
+//const http = require('./http'); //would find http locally in the same folder
+
+//creating a server
+const server = http.createServer(function (req, res) {
+  requestHandler(req, res);
+});
+
+server.listen(3000);
+
+//this example was written without express. Would be alot easier if you used express for requests and responses.
